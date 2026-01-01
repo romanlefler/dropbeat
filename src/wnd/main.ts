b698aaa75462c270dcdb907095e32a4c861f7c7a
Roman Lefler <romanlefler@proton.me>
2025-12-31 22:38:10 -0600

    feat(wnd): Script to create fullscreen GTK4 window

---
+++ src/wnd/main.ts
@@ -0,0 +1,69 @@
+/*
+    Copyright 2025 Roman Lefler
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+ * This "wnd" part of the project controls the fullscreen player that pops up
+ * when clicking the card.
+ *
+ * This script can be executed via:
+ * gjs -m ./wnd/main.js
+ * from the build directory.
+ */
+
+import Gio from "gi://Gio";
+import Gtk from "gi://Gtk?version=4.0";
+
+const APP_ID = "com.roman.Dropbeat.Wnd";
+
+function main(argv: string[])
+{
+    const app = new Gtk.Application(
+    {
+        application_id: APP_ID,
+        flags: Gio.ApplicationFlags.FLAGS_NONE
+    });
+
+    app.connect("activate", () =>
+    {
+        const wnd = new Gtk.ApplicationWindow(
+        {
+            application: app,
+            title: "Dropbeat",
+        });
+
+        const box = new Gtk.Box(
+        {
+            orientation: Gtk.Orientation.VERTICAL,
+        });
+        box.append(new Gtk.Label({ label: "Dropbeat Fullscreen" }));
+
+        wnd.set_child(box);
+
+        wnd.connect("close-request", () =>
+        {
+            app.quit();
+            return false;
+        });
+
+        wnd.present();
+        wnd.fullscreen();
+    });
+
+    return app.run([]);
+}
+
+main(ARGV);
